8a1adbcfc3b53528bc0a1938ed2274c5686b59f9
samuel normand <samuel.normand@outlook.fr>
2023-09-07 17:58:44 +0200

    Add gif

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import numpy as np
 import requests
 from PIL import Image
 from io import BytesIO
-st.title("Image Uploader")
+st.title("Autonomous.ai")
 uploaded_image = st.file_uploader("Upload a JPG", type=["jpg"])
+placeholder = st.empty()
+
 if uploaded_image:
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+    placeholder.image('https://media.tenor.com/mzOGTg03U4cAAAAC/mr-bean-attente.gif')
     response = requests.post("https://docker-auto-ai-bluesky-ku6cxn3xga-ew.a.run.app/predict/image", files={"file": uploaded_image})
 # Check if the request was successful
     if response.status_code == 200:
@@ -15,6 +18,8 @@ if uploaded_image:
         # Open the image using Pillow (PIL)
         img = Image.open(image_bytes)
         # Display the image in Streamlit
+        st.title('Prediction of our model:')
         st.image(img, caption='Image Caption', use_column_width=True)
+        placeholder.empty()
     else:
         st.error('Failed to fetch image. Status code: {}'.format(response.status_code))
