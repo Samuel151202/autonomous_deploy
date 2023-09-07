d8bd4e3c07ff39c52ae997904ca6a2d120f09b0b
samuel normand <samuel.normand@outlook.fr>
2023-09-07 16:16:12 +0200

    Fix bluesky

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Image Uploader")
 uploaded_image = st.file_uploader("Upload a JPG", type=["jpg"])
 if uploaded_image:
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
-    response = requests.post("https://docker-auto-ai-bluesky-ku6cxn3xga-ew.a.run.app", files={"file": uploaded_image})
+    response = requests.post("https://docker-auto-ai-bluesky-ku6cxn3xga-ew.a.run.app/predict/image", files={"file": uploaded_image})
 # Check if the request was successful
     if response.status_code == 200:
         # Read the image data from the response
